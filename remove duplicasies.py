8cc0727e275040f260c0f549e9b7a4cfc7578ce4
premgolani <50552367+premgolani@users.noreply.github.com>
2019-07-28 11:55:53 +0530

    Update remove duplicasies.py

--- remove duplicasies.py
+++ remove duplicasies.py
@@ -1,3 +1,3 @@
 mylist=[1,2,3,1,4,3]
-mylist=list(dict.fromkeys(mylist))
+mylist=list(dict.fromkeys(mylist)) #creating dictionary to remove duplicate strings and adding them in a list
 print(mylist)
